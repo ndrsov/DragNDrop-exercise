f411921e3eb5a884890447e16b4c8a255cdb31b8
Andres Soria <andres.zov@gmail.com>
2022-09-07 14:19:25 -0500

    Add correct classes depending on the state of the drag event

--- app.js
+++ app.js
@@ -12,20 +12,25 @@ for (const empty of empties) {
 }
 
 function dragStart() {
-  console.log("drag start");
+  this.className += " hold";
+  setTimeout(() => {
+    this.className = "invisible";
+  }, 0);
 }
 function dragEnd() {
-  console.log("drag End");
+  this.className = "fill";
 }
-function dragOver() {
-  console.log("drag Over");
+function dragOver(e) {
+  e.preventDefault();
 }
-function dragEnter() {
-  console.log("drag Enter");
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
 }
 function dragLeave() {
-  console.log("drag Leave");
+  this.className = "empty";
 }
 function dragDrop() {
-  console.log("drag Drop");
+  this.className = "empty";
+  this.append(fill);
 }
